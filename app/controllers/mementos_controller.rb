d3b5f60d717686552e1fa8e8e1a3d5bbf5e8d2fa
Daniel Schwarz <danielschvarz@hotmail.com>
2013-02-01 09:04:33 -0500

    Logic fix

--- app/controllers/mementos_controller.rb
+++ app/controllers/mementos_controller.rb
@@ -10,9 +10,14 @@ class MementosController < ApplicationController
   def show
     @memento = Memento.new(body: "Someone died a few seconds ago - it wasn't you :)")
     if(!Memento.last.nil?)
+      timer = 0
      begin
+       timer++
        @memento = Memento.find_by_id(rand(Memento.last.id));
-     end while @memento.nil?
+     end while @memento.nil? and timer < 10
+    end
+    if(@memento.nil?)
+      @memento = Memento.new(body: "Someone died a few seconds ago - it wasn't you :)")
     end
     
   end
